fb461023330a78557c92912e5a008fa5cf17d4e6
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2017-03-11 17:45:40 +0000

    VS2015 update and a screenshot

--- IpVideoSource/Sample/.NET/PlayJpegLocationSharp/Program.cs
+++ IpVideoSource/Sample/.NET/PlayJpegLocationSharp/Program.cs
@@ -13,15 +13,12 @@ namespace PlayJpegLocationSharp
         static void Main(string[] args)
         {
             FilterGraph filterGraph = new FilterGraph();
-            IJpegVideoSourceFilter sourceFilter = Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("{A8DA2ECB-DEF6-414D-8CE2-E651640DBA4F}"))) as IJpegVideoSourceFilter;
+            IJpegVideoSourceFilter sourceFilter = new JpegVideoSourceFilter() as IJpegVideoSourceFilter;
             IBaseFilter sourceBaseFilter = sourceFilter as IBaseFilter;
             (filterGraph as IFilterGraph2).AddFilter(sourceBaseFilter, "Source");
-            //sourceFilter.Location = @"http://p.viewnetcam.com:60001/nphMotionJpeg?Resolution=640x480&Quality=Standard";
-            //sourceFilter.Width = 640;
-            //sourceFilter.Height = 480;
-            sourceFilter.Location = @"http://192.168.88.52/nph-jpeg.cgi";
-            sourceFilter.Width = 1280;
-            sourceFilter.Height = 960;
+            sourceFilter.Location = @"http://192.168.88.21/nph-mjpeg.cgi";
+            sourceFilter.Width = 800;
+            sourceFilter.Height = 448;
             IPin pin = DsFindPin.ByDirection(sourceBaseFilter, PinDirection.Output, 0);
             (filterGraph as IFilterGraph2).Render(pin);
             (filterGraph as IMediaControl).Run();
