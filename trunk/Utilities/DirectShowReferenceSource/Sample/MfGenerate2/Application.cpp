d98292711b97cf5a96bf1326d3bdf96c2c372b8c
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2017-07-13 15:02:57 +0000

    Added audio MF source; added audio track support in MfGenerate2 sample application

--- trunk/Utilities/DirectShowReferenceSource/Sample/MfGenerate2/Application.cpp
+++ trunk/Utilities/DirectShowReferenceSource/Sample/MfGenerate2/Application.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////
-// Copyright (C) Roman Ryltsov, 2015-2016
+// Copyright (C) Roman Ryltsov, 2015-2017
 // Created by Roman Ryltsov roman@alax.info
 //
 // A permission to use the source code is granted as long as reference to 
@@ -36,7 +36,7 @@ public:
 		CComPtr<IMFSourceResolver> pSourceResolver;
 		__C(MFCreateSourceResolver(&pSourceResolver));
 		#pragma region Media Source
-		CComQIPtr<IMFMediaSource> pMediaSource;
+		CComPtr<IMFMediaSource> pMediaSource;
 		//{
 		//	static const LPCTSTR g_pszPath = 
 		//		_T("D:\\Projects\\Alax.Info\\Repository-Private\\Utilities\\DirectShow\\ReferenceSource\\_Media\\720p50-Small-V.mp4");
@@ -47,16 +47,51 @@ public:
 		//	pMediaSource = pMediaSourceUnknown;
 		//	__D(pMediaSource, E_NOINTERFACE);
 		//}
+		CComPtr<IMFMediaSource> pVideoMediaSource, pAudioMediaSource;
+		const SIZE g_Extent = { 640, 360 };
+		const LONG g_nFrameRateNumerator = 30000;
+		const LONG g_nFrameRateDenominator = 1001;
+		const LONG g_nDuration = 1 * 60; // minutes
+		const LONG g_nVideoBitrate = (2 << 10) * 1000; // 2 MBps
+		const LONG g_nSampleRate = 48000;
+		const LONG g_nChannelCount = 1;
+		const LONG g_nBitDepth = 16;
+		const LONG g_nAudioBitrate = 20 * 1000;
+		#pragma region Video
+		if(TRUE)
 		{
 			using namespace AlaxInfoDirectShowReferenceSource;
-			CComPtr<IVideoMediaSource> pVideoMediaSource;
-			__C(pVideoMediaSource.CoCreateInstance(__uuidof(VideoMediaSource)));
-			__C(pVideoMediaSource->SetMediaType(640, 480, CComVariant(_PersistHelper::StringFromIdentifier(MEDIASUBTYPE_ARGB32))));
-			//__C(pVideoMediaSource->SetMediaTypeAspectRatio(...));
-			__C(pVideoMediaSource->SetMediaTypeRate(10000, 1000));
-			__C(pVideoMediaSource->put_Duration(5.0));
-			pMediaSource = pVideoMediaSource;
+			CComPtr<IVideoMediaSource> pSource;
+			__C(pSource.CoCreateInstance(__uuidof(VideoMediaSource)));
+			__C(pSource->SetMediaType(g_Extent.cx, g_Extent.cy, CComVariant(_PersistHelper::StringFromIdentifier(MEDIASUBTYPE_ARGB32))));
+			//__C(pSource->SetMediaTypeAspectRatio(...));
+			__C(pSource->SetMediaTypeRate(g_nFrameRateNumerator, g_nFrameRateDenominator));
+			__C(pSource->put_Duration((DOUBLE) g_nDuration));
+			pVideoMediaSource = pSource;
 		}
+		#pragma endregion 
+		#pragma region Audio
+		if(TRUE)
+		{
+			using namespace AlaxInfoDirectShowReferenceSource;
+			CComPtr<IAudioMediaSource> pSource;
+			__C(pSource.CoCreateInstance(__uuidof(AudioMediaSource)));
+			__C(pSource->SetMediaType(NULL, g_nSampleRate, g_nChannelCount, g_nBitDepth));
+			__C(pSource->put_Duration((DOUBLE) g_nDuration));
+			pAudioMediaSource = pSource;
+		}
+		#pragma endregion 
+		__D(pVideoMediaSource || pAudioMediaSource, E_UNNAMED);
+		if(pVideoMediaSource && pAudioMediaSource)
+		{
+		    CComPtr<IMFCollection> pCollection;
+		    __C(MFCreateCollection(&pCollection));
+	        __C(pCollection->AddElement(pVideoMediaSource));
+	        __C(pCollection->AddElement(pAudioMediaSource));
+	        __C(MFCreateAggregateSource(pCollection, &pMediaSource));
+		} else
+			pMediaSource = pVideoMediaSource ? pVideoMediaSource : pAudioMediaSource;
+		_A(pMediaSource);
 		#pragma endregion 
 		CPath sFilePath;
 		{
@@ -71,25 +106,50 @@ public:
 		//       Tutorial: Encoding an MP4 File https://msdn.microsoft.com/en-us/library/ff819476
 		MF::CTopology pTopology;
 		//pTopology.Create();
+		#pragma region Profile
 		CComPtr<IMFTranscodeProfile> pTranscodeProfile;
 		{
 			__C(MFCreateTranscodeProfile(&pTranscodeProfile));
+			#pragma region Video
+			if(pVideoMediaSource)
 			{
 				MF::CAttributes pAttributes;
 				pAttributes.Create(8);
-				pAttributes[MF_MT_SUBTYPE] = MFVideoFormat_H264;
-				pAttributes[MF_MT_AVG_BITRATE] = (UINT32) (5 << 10) * 1000; // 12 MBps
-				pAttributes[MF_MT_FRAME_SIZE].SetSize(640, 480);
-				pAttributes[MF_MT_FRAME_RATE].SetRatio(10000, 1000);
+				pAttributes[MF_MT_SUBTYPE] = 
+					MFVideoFormat_H264;
+					//MFVideoFormat_HEVC;
+				pAttributes[MF_MT_AVG_BITRATE] = (UINT32) g_nVideoBitrate;
+				pAttributes[MF_MT_FRAME_SIZE].SetSize(g_Extent.cx, g_Extent.cy);
+				pAttributes[MF_MT_FRAME_RATE].SetRatio(g_nFrameRateNumerator, g_nFrameRateDenominator);
 				__C(pTranscodeProfile->SetVideoAttributes(pAttributes));
+				pAttributes.Trace();
 			}
+			#pragma endregion
+			#pragma region Audio
+			if(pAudioMediaSource)
 			{
+				// NOTE: The following audio attributes can be set: https://msdn.microsoft.com/en-us/library/windows/desktop/dd369143
+				//   Audio Media Types 
+				//   MF_TRANSCODE_DONOT_INSERT_ENCODER 
+				//   MF_TRANSCODE_ENCODINGPROFILE 
+				//   MF_TRANSCODE_QUALITYVSSPEED
 				MF::CAttributes pAttributes;
-				pAttributes.Create(1);
-				pAttributes[MF_TRANSCODE_CONTAINERTYPE] = MFTranscodeContainerType_MPEG4;
-				__C(pTranscodeProfile->SetContainerAttributes(pAttributes));
+				pAttributes.Create(8);
+				pAttributes[MF_MT_SUBTYPE] = MFAudioFormat_AAC;
+				pAttributes[MF_MT_AUDIO_SAMPLES_PER_SECOND] = (UINT32) g_nSampleRate;
+				pAttributes[MF_MT_AUDIO_NUM_CHANNELS] = (UINT32) g_nChannelCount;
+				pAttributes[MF_MT_AUDIO_BITS_PER_SAMPLE] = (UINT32) g_nBitDepth;
+				pAttributes[MF_MT_AUDIO_AVG_BYTES_PER_SECOND] = (UINT32) g_nAudioBitrate;
+				__C(pTranscodeProfile->SetAudioAttributes(pAttributes));
+				pAttributes.Trace();
 			}
+			#pragma endregion
+			MF::CAttributes pAttributes;
+			pAttributes.Create(1);
+			pAttributes[MF_TRANSCODE_CONTAINERTYPE] = MFTranscodeContainerType_MPEG4;
+			__C(pTranscodeProfile->SetContainerAttributes(pAttributes));
 		}
+		#pragma endregion
 		__C(MFCreateTranscodeTopology(pMediaSource, CT2CW((LPCTSTR) sFilePath), pTranscodeProfile, &pTopology.m_p));
 		pTopology.Trace();
 		#pragma endregion 
@@ -101,14 +161,23 @@ public:
 		{
 			MF::CMediaEvent pMediaEvent;
 			__C(pMediaSession->GetEvent(0, &pMediaEvent));
+			pMediaEvent.Trace();
 			MediaEventType Type;
 			__C(pMediaEvent->GetType(&Type));
 			HRESULT nStatus;
 			__C(pMediaEvent->GetStatus(&nStatus));
-			_Z4(atlTraceGeneral, 4, _T("Type %s, nStatus %s\n"), MF::CMediaEvent::FormatType(Type), MF::FormatResult(nStatus));
 			switch(Type)
 			{
 			case MESessionTopologySet:
+				{
+					MF::CPropVariant vValue;
+					if(SUCCEEDED(pMediaEvent->GetValue(&vValue)) && vValue.vt == VT_UNKNOWN)
+					{
+						MF::CTopology pTopology;
+						pTopology.m_p = CComQIPtr<IMFTopology>(vValue.punkVal);
+						pTopology.Trace();
+					}
+				}
 				break;
 			case MESessionTopologyStatus:
 				{
