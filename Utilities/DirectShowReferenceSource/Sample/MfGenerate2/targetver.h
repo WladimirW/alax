04928c6296f2ef53ba6e9c2dcfae517fcf0fc52e
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2017-07-13 15:02:57 +0000

    Added audio MF source; added audio track support in MfGenerate2 sample application

--- Utilities/DirectShowReferenceSource/Sample/MfGenerate2/targetver.h
+++ Utilities/DirectShowReferenceSource/Sample/MfGenerate2/targetver.h
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////
-// Copyright (C) Roman Ryltsov, 2015
+// Copyright (C) Roman Ryltsov, 2015-2017
 // Created by Roman Ryltsov roman@alax.info
 //
 // A permission to use the source code is granted as long as reference to 
@@ -12,4 +12,5 @@
 // If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
-#include <SDKDDKVer.h>
+#include <winsdkver.h>
+#include <sdkddkver.h>
