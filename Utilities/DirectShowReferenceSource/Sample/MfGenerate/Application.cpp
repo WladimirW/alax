03b8ab733922fc973e5ffa80272974706b71222e
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2018-02-23 09:59:55 +0000

    Sync and added MfGenerateFlac sample

--- Utilities/DirectShowReferenceSource/Sample/MfGenerate/Application.cpp
+++ Utilities/DirectShowReferenceSource/Sample/MfGenerate/Application.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////
-// Copyright (C) Roman Ryltsov, 2015-2017
+// Copyright (C) Roman Ryltsov, 2015-2018
 // Created by Roman Ryltsov roman@alax.info
 //
 // A permission to use the source code is granted as long as reference to 
@@ -37,6 +37,7 @@ public:
 		__C(MFCreateSourceResolver(&pSourceResolver));
 		#pragma region Media Source
 		CComQIPtr<IMFMediaSource> pMediaSource;
+
 		//{
 		//	static const LPCTSTR g_pszPath = 
 		//		_T("D:\\Projects\\Alax.Info\\Repository-Private\\Utilities\\DirectShow\\ReferenceSource\\_Media\\720p50-Small-V.mp4");
@@ -67,16 +68,58 @@ public:
 		//const LONG g_nBitrate = (5 << 10) * 1000; // 5 MBps
 
 		const SIZE g_Extent = { 1920, 1080 };
-		const LONG g_nFrameRateNumerator = 25 * 1000; // frames/second
+		const LONG g_nFrameRateNumerator = 50 * 1000; // frames/second
 		const LONG g_nFrameRateDenominator = 1000;
 		const LONG g_nDuration = 1 * 60; // minutes
-		const LONG g_nBitrate = (2 << 10) * 1000; // 2 MBps
+		const LONG g_nBitrate = (10 * 1000) * 1000; // 10 MBps
 
 		//const SIZE g_Extent = { 4096, 2304 };
 		//const LONG g_nFrameRateNumerator = 50;
 		//const LONG g_nFrameRateDenominator = 1;
 		//const LONG g_nDuration = 5 * 60; // 5 minutes
 		//const LONG g_nBitrate = (2 << 10) * 1000; // 2 MBps
+
+		#pragma region DXGI Device Manager
+		static SIZE_T g_nDxgiAdapterIndex = 0;
+		MF::CDxgiDeviceManager pDxgiDeviceManager;
+		{
+			#pragma region DXGI Adapter
+			DXGI::CFactory2 pFactory2;
+			pFactory2.DebugCreate();
+			CRoArrayT<DXGI::CAdapter1> AdapterArray;
+			_A(pFactory2.EnumerateAdapters(AdapterArray) == 3);
+			DXGI::CAdapter1& pAdapter1 = AdapterArray[g_nDxgiAdapterIndex];
+			#pragma region D3D11 Device
+			CComPtr<ID3D11Device> pDevice;
+			UINT nFlags = D3D11_CREATE_DEVICE_VIDEO_SUPPORT;
+			#if defined(_DEBUG)
+				nFlags |= D3D11_CREATE_DEVICE_DEBUG;
+			#endif // defined(_DEBUG)
+			static const D3D_FEATURE_LEVEL g_pFeatureLevels[] = 
+			{
+				D3D_FEATURE_LEVEL_12_1,
+				D3D_FEATURE_LEVEL_12_0,
+				D3D_FEATURE_LEVEL_11_1,
+				D3D_FEATURE_LEVEL_11_0,
+				D3D_FEATURE_LEVEL_10_1,
+				D3D_FEATURE_LEVEL_10_0,
+				D3D_FEATURE_LEVEL_9_3,
+				D3D_FEATURE_LEVEL_9_2,
+				D3D_FEATURE_LEVEL_9_1,
+			};
+			D3D_FEATURE_LEVEL FeatureLevel;
+			CComPtr<ID3D11DeviceContext> pDeviceContext;
+			// SUGG: Reuse CVideoInput's CreateDevice doing D3D11CreateDevice
+			__C(D3D11CreateDevice(pAdapter1, D3D_DRIVER_TYPE_UNKNOWN, NULL, nFlags, g_pFeatureLevels, DIM(g_pFeatureLevels), D3D11_SDK_VERSION, &pDevice, &FeatureLevel, &pDeviceContext));
+			const CComQIPtr<ID3D11Multithread> pMultithread = pDevice;
+			__D(pMultithread, E_NOINTERFACE);
+			pMultithread->SetMultithreadProtected(TRUE);
+			#pragma endregion 
+			#pragma endregion 
+			pDxgiDeviceManager.Create();
+			pDxgiDeviceManager.Reset(pDevice);
+		}
+		#pragma endregion 
 
 		{
 			using namespace AlaxInfoDirectShowReferenceSource;
@@ -90,7 +133,16 @@ public:
 		}
 		#pragma endregion 
 		CComPtr<IMFSourceReader> pSourceReader;
-		__C(MFCreateSourceReaderFromMediaSource(pMediaSource, NULL, &pSourceReader));
+		{
+			MF::CAttributes pAttributes;
+			if(pDxgiDeviceManager)
+			{
+				pAttributes.Create(4);
+				pAttributes[MF_READWRITE_D3D_OPTIONAL] = (UINT32) 0;
+				pAttributes[MF_SOURCE_READER_D3D_MANAGER] = (IMFDXGIDeviceManager*) pDxgiDeviceManager;
+			}
+			__C(MFCreateSourceReaderFromMediaSource(pMediaSource, pAttributes, &pSourceReader));
+		}
 		MF::CMediaType pMediaType;
 		__C(pSourceReader->GetCurrentMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM, &pMediaType));
 		pMediaType.Trace();
@@ -98,39 +150,54 @@ public:
 		GetLocalTime(&Time);
 		const CString sFileName = AtlFormatString(_T("%04d%02d%02d-%02d%02d%02d.mp4"), Time.wYear, Time.wMonth, Time.wDay, Time.wHour, Time.wMinute, Time.wSecond);
 		CComPtr<IMFSinkWriter> pSinkWriter;
-		__C(MFCreateSinkWriterFromURL(Combine(GetPathDirectory(GetModulePath()), sFileName), NULL, NULL, &pSinkWriter));
+		{
+			MF::CAttributes pAttributes;
+			if(pDxgiDeviceManager)
+			{
+				pAttributes.Create(4);
+				pAttributes[MF_READWRITE_D3D_OPTIONAL] = (UINT32) 0;
+				pAttributes[MF_SINK_WRITER_D3D_MANAGER] = (IMFDXGIDeviceManager*) pDxgiDeviceManager;
+			}
+			__C(MFCreateSinkWriterFromURL(Combine(GetPathDirectory(GetModulePath()), sFileName), NULL, pAttributes, &pSinkWriter));
+		}
 		MF::CMediaType pWriterMediaType;
 		pWriterMediaType.Create();
 		// NOTE: H.264 Video Encoder https://msdn.microsoft.com/en-us/library/windows/desktop/dd797816
 		pWriterMediaType[MF_MT_MAJOR_TYPE] = MFMediaType_Video;
 		pWriterMediaType[MF_MT_SUBTYPE] = MFVideoFormat_H264;
+		pWriterMediaType.CopyFrom(pMediaType, MF_MT_FRAME_RATE);
+		pWriterMediaType.CopyFrom(pMediaType, MF_MT_FRAME_SIZE);
+		pWriterMediaType.CopyFrom(pMediaType, MF_MT_INTERLACE_MODE);
+		pWriterMediaType.CopyFrom(pMediaType, MF_MT_PIXEL_ASPECT_RATIO);
 		pWriterMediaType[MF_MT_AVG_BITRATE] = (UINT32) g_nBitrate;
-		pWriterMediaType[MF_MT_FRAME_RATE] = pMediaType.GetUINT64(MF_MT_FRAME_RATE);
-		pWriterMediaType[MF_MT_FRAME_SIZE] = pMediaType.GetUINT64(MF_MT_FRAME_SIZE);
-		pWriterMediaType[MF_MT_INTERLACE_MODE] = pMediaType.GetUINT32(MF_MT_INTERLACE_MODE);
-		pWriterMediaType[MF_MT_PIXEL_ASPECT_RATIO] = pMediaType.GetUINT64(MF_MT_PIXEL_ASPECT_RATIO);
-
-	//pWriterMediaType[MF_MT_MPEG2_PROFILE] = (UINT32) eAVEncH264VProfile_High;
-
+		//pWriterMediaType[MF_MT_MPEG2_PROFILE] = (UINT32) eAVEncH264VProfile_High;
 		pWriterMediaType.Trace();
 		DWORD nWriterStreamIndex;
 		__C(pSinkWriter->AddStream(pWriterMediaType, &nWriterStreamIndex));
 		// NOTE: Sink Writer Attributes https://msdn.microsoft.com/en-us/library/windows/desktop/dd389284
+
+		//class __declspec(uuid("4E498B6F-7DCA-403E-A523-912A73943341")) NvEncoderTransform;
+		//MFT_REGISTER_TYPE_INFO pInputTypeInformation[] = { { MFMediaType_Video, MFVideoFormat_ARGB32 } };
+		//MFT_REGISTER_TYPE_INFO pOutputTypeInformation[] = { { MFMediaType_Video, MFVideoFormat_H264 } };
+		//__C(MFTRegisterLocalByCLSID(__uuidof(NvEncoderTransform), MFT_CATEGORY_VIDEO_ENCODER, L"Rainway H.264 Encoder for NVIDIA HW", MFT_ENUM_FLAG_ASYNCMFT, DIM(pInputTypeInformation), pInputTypeInformation, DIM(pOutputTypeInformation), pOutputTypeInformation));
 		{
 			MF::CAttributes pAttributes;
 			pAttributes.Create(10);
 			pAttributes[MF_READWRITE_ENABLE_HARDWARE_TRANSFORMS] = (UINT32) 1;
 			__C(pSinkWriter->SetInputMediaType(nWriterStreamIndex, pMediaType, pAttributes));
 		}
+		MF::CSinkWriterHelper::TraceTransformsT<MF::CSinkWriterHelper::CVideoTraceTraits>(pSinkWriter);
 
-	//CRoArrayT<CComPtr<IMFTransform>> TransformArray;
-	//MF::CSinkWriterHelper::GetTransforms(pSinkWriter, nWriterStreamIndex, TransformArray);
-	//_A(!TransformArray.IsEmpty());
-	//MF::CCodecApi pCodecApi(TransformArray[TransformArray.GetCount() - 1]);
-	//// NOTE: H.264 Video Encoder https://msdn.microsoft.com/en-us/library/windows/desktop/dd797816
-	//pCodecApi[CODECAPI_AVEncMPVGOPSize] = (UINT32) 16;
-	//pCodecApi[CODECAPI_AVEncMPVDefaultBPictureCount] = (UINT32) 3;
+		CRoArrayT<CComPtr<IMFTransform>> TransformArray;
+		MF::CSinkWriterHelper::GetTransforms(pSinkWriter, nWriterStreamIndex, TransformArray);
+		_A(!TransformArray.IsEmpty());
+		MF::CCodecApi pCodecApi(TransformArray[TransformArray.GetCount() - 1]);
+		pCodecApi[CODECAPI_AVEncCommonRateControlMode] = (UINT32) eAVEncCommonRateControlMode_CBR;
+		pCodecApi[CODECAPI_AVEncCommonMeanBitRate] = (UINT32) g_nBitrate;
+		pCodecApi[CODECAPI_AVEncMPVGOPSize] = (UINT32) 1;
+		//pCodecApi[CODECAPI_AVEncMPVDefaultBPictureCount] = (UINT32) 0;
 
+		const ULONG nTimeA = GetTickCount();
 		__C(pSinkWriter->BeginWriting());
 		for(; ; )
 		{
@@ -142,6 +209,24 @@ public:
 			if(pSample)
 			{
 				pSample.Trace();
+				#pragma region Re-Configuration
+				#if defined(_DEBUG)
+					static INT g_nState = 0;
+					if(!g_nState && pSample.GetTime() >= 30000 * 10000i64)
+					{
+						g_nState = 1;
+						const CComQIPtr<IMFSinkWriterEncoderConfig> pSinkWriterEncoderConfig = pSinkWriter;
+						__D(pSinkWriterEncoderConfig, E_NOINTERFACE);
+						MF::CAttributes pAttributes;
+						pAttributes.Create(4);
+						//pAttributes[CODECAPI_AVEncCommonRateControlMode] = (UINT32) eAVEncCommonRateControlMode_CBR;
+						pAttributes[CODECAPI_AVEncCommonMeanBitRate] = (UINT32) g_nBitrate / 4;
+						// NOTE: MF_SOURCE_READER_FIRST_VIDEO_STREAM results in MF_E_INVALIDSTREAMNUMBER
+						__C(pSinkWriterEncoderConfig->PlaceEncodingParameters(0, pAttributes));
+						// SUGG: Check IMFSinkWriterEncoderConfig::SetTargetMediaType as well
+					}
+				#endif // defined(_DEBUG)
+				#pragma endregion 
 				__C(pSinkWriter->WriteSample(nWriterStreamIndex, pSample));
 			}
 			if(nStreamFlags & MF_SOURCE_READERF_ENDOFSTREAM)
@@ -151,6 +236,8 @@ public:
 			}
 		}
 		__C(pSourceReader->Flush(MF_SOURCE_READER_ALL_STREAMS));
+		const ULONG nTimeB = GetTickCount();
+		_tprintf(_T("Elapsed Time: %.2f seconds\n"), (DOUBLE) (nTimeB - nTimeA) / 1E3);
 		__C(pSinkWriter->Finalize());
 	}
 };
